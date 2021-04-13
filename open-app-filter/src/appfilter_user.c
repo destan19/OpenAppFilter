29f297997ffdfa85c9321c39e63e46b4f52b5976
Derry <destan19@126.com>
2021-04-13 02:31:15 -0700

    Format code

--- open-app-filter/src/appfilter_user.c
+++ open-app-filter/src/appfilter_user.c
@@ -1,23 +1,23 @@
 /*
-  Copyright (C) 2020 Derry <destan19@126.com>
+Copyright (C) 2020 Derry <destan19@126.com>
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 #include <unistd.h>
 #include <stdlib.h>
@@ -35,48 +35,56 @@ dev_node_t *dev_hash_table[MAX_DEV_NODE_HASH_SIZE];
 int g_cur_user_num = 0;
 unsigned int hash_mac(unsigned char *mac)
 {
-	if (!mac)
-		return 0;
-	else
-		return mac[0] & (MAX_DEV_NODE_HASH_SIZE - 1);
+    if (!mac)
+        return 0;
+    else
+        return mac[0] & (MAX_DEV_NODE_HASH_SIZE - 1);
 }
-int get_timestamp(void){
-	struct timeval cur_time;
-	gettimeofday(&cur_time, NULL);
-	return cur_time.tv_sec;
-}
-
-int hash_appid(int appid){
-	return appid % (MAX_VISIT_HASH_SIZE - 1);
+int get_timestamp(void)
+{
+    struct timeval cur_time;
+    gettimeofday(&cur_time, NULL);
+    return cur_time.tv_sec;
 }
 
-void add_visit_info_node(visit_info_t **head, visit_info_t *node){
-	if (*head == NULL){
-		*head = node;
-	}
-	else{
-		node->next = *head;
-		*head = node;
-	}
+int hash_appid(int appid)
+{
+    return appid % (MAX_VISIT_HASH_SIZE - 1);
 }
 
-void init_dev_node_htable(){
+void add_visit_info_node(visit_info_t **head, visit_info_t *node)
+{
+    if (*head == NULL)
+    {
+        *head = node;
+    }
+    else
+    {
+        node->next = *head;
+        *head = node;
+    }
+}
+
+void init_dev_node_htable()
+{
     int i;
-    for (i = 0; i < MAX_DEV_NODE_HASH_SIZE; i++){
+    for (i = 0; i < MAX_DEV_NODE_HASH_SIZE; i++)
+    {
         dev_hash_table[i] = NULL;
     }
 }
 
-
-
-dev_node_t *add_dev_node(char *mac){
+dev_node_t *add_dev_node(char *mac)
+{
     unsigned int hash = 0;
-	if (g_cur_user_num >= MAX_SUPPORT_USER_NUM){
-		printf("error, user num reach max %d\n", g_cur_user_num);
-		return NULL;
-	}
+    if (g_cur_user_num >= MAX_SUPPORT_USER_NUM)
+    {
+        printf("error, user num reach max %d\n", g_cur_user_num);
+        return NULL;
+    }
     hash = hash_mac(mac);
-    if (hash >= MAX_DEV_NODE_HASH_SIZE){
+    if (hash >= MAX_DEV_NODE_HASH_SIZE)
+    {
         printf("hash code error %d\n", hash);
         return NULL;
     }
@@ -84,30 +92,35 @@ dev_node_t *add_dev_node(char *mac){
     if (!node)
         return NULL;
     strncpy(node->mac, mac, sizeof(node->mac));
-	node->online = 1;
-	node->online_time = get_timestamp();
+    node->online = 1;
+    node->online_time = get_timestamp();
     if (dev_hash_table[hash] == NULL)
         dev_hash_table[hash] = node;
-    else{
+    else
+    {
         node->next = dev_hash_table[hash];
         dev_hash_table[hash] = node;
     }
-	g_cur_user_num++;
+    g_cur_user_num++;
     printf("add mac:%s to htable[%d]....success\n", mac, hash);
     return node;
 }
 
-dev_node_t *find_dev_node(char *mac){
+dev_node_t *find_dev_node(char *mac)
+{
     unsigned int hash = 0;
     dev_node_t *p = NULL;
     hash = hash_mac(mac);
-    if (hash >= MAX_DEV_NODE_HASH_SIZE){
+    if (hash >= MAX_DEV_NODE_HASH_SIZE)
+    {
         printf("hash code error %d\n", hash);
         return NULL;
     }
     p = dev_hash_table[hash];
-    while(p){
-        if (0 == strncmp(p->mac, mac, sizeof(p->mac))){
+    while (p)
+    {
+        if (0 == strncmp(p->mac, mac, sizeof(p->mac)))
+        {
             return p;
         }
         p = p->next;
@@ -115,181 +128,224 @@ dev_node_t *find_dev_node(char *mac){
     return NULL;
 }
 
-void dev_foreach(void *arg, iter_func iter){
-	int i, j;
-	dev_node_t *node = NULL;
+void dev_foreach(void *arg, iter_func iter)
+{
+    int i, j;
+    dev_node_t *node = NULL;
 
-    for (i = 0;i < MAX_DEV_NODE_HASH_SIZE; i++){
+    for (i = 0; i < MAX_DEV_NODE_HASH_SIZE; i++)
+    {
         dev_node_t *node = dev_hash_table[i];
-		while(node){
+        while (node)
+        {
             iter(arg, node);
-			node = node->next;
-		}
+            node = node->next;
+        }
     }
 }
 
-
-char * format_time(int timetamp){
-	char time_buf[64] = {0};
-	time_t seconds = timetamp;
-	struct tm *auth_tm = localtime(&seconds); 
-	strftime(time_buf, sizeof(time_buf), "%Y %m %d %H:%M:%S", auth_tm);
-	return strdup(time_buf);
+char *format_time(int timetamp)
+{
+    char time_buf[64] = {0};
+    time_t seconds = timetamp;
+    struct tm *auth_tm = localtime(&seconds);
+    strftime(time_buf, sizeof(time_buf), "%Y %m %d %H:%M:%S", auth_tm);
+    return strdup(time_buf);
 }
 
-
-void update_dev_hostname(void){
+void update_dev_hostname(void)
+{
     char line_buf[256] = {0};
     char hostname_buf[128] = {0};
     char mac_buf[32] = {0};
-	char ip_buf[32] = {0};
-	
+    char ip_buf[32] = {0};
+
     FILE *fp = fopen("/tmp/dhcp.leases", "r");
-	if (!fp){
-		printf("open dhcp lease file....failed\n");
-		return;
-	}
-    while(fgets(line_buf, sizeof(line_buf), fp)){
-		if (strlen(line_buf) <= 16)
-			continue;
+    if (!fp)
+    {
+        printf("open dhcp lease file....failed\n");
+        return;
+    }
+    while (fgets(line_buf, sizeof(line_buf), fp))
+    {
+        if (strlen(line_buf) <= 16)
+            continue;
         sscanf(line_buf, "%*s %s %s %s", mac_buf, ip_buf, hostname_buf);
-		dev_node_t *node = find_dev_node(mac_buf);
-		if (!node)
-			continue;
-		if (strlen(hostname_buf) > 0){
-			strncpy(node->hostname, hostname_buf, sizeof(node->hostname));
-		}
-    }   
+        dev_node_t *node = find_dev_node(mac_buf);
+        if (!node)
+            continue;
+        if (strlen(hostname_buf) > 0)
+        {
+            strncpy(node->hostname, hostname_buf, sizeof(node->hostname));
+        }
+    }
     fclose(fp);
 }
 
-void clean_dev_online_status(void){
-	int i;
-	for (i = 0;i < MAX_DEV_NODE_HASH_SIZE; i++){
+void clean_dev_online_status(void)
+{
+    int i;
+    for (i = 0; i < MAX_DEV_NODE_HASH_SIZE; i++)
+    {
         dev_node_t *node = dev_hash_table[i];
-		while(node){
-			node->online = 0;
-			node->offline_time = get_timestamp();
-			node = node->next;
-		}
+        while (node)
+        {
+            node->online = 0;
+            node->offline_time = get_timestamp();
+            node = node->next;
+        }
     }
 }
 
 /*
-    Id   Mac                  Ip                  
-    1    10:bf:48:37:0c:94    192.168.66.244 
+Id   Mac                  Ip                  
+1    10:bf:48:37:0c:94    192.168.66.244 
 */
-void check_dev_expire(void){
+void check_dev_expire(void)
+{
     char line_buf[256] = {0};
     char mac_buf[32] = {0};
     char ip_buf[32] = {0};
-    
+
     FILE *fp = fopen("/proc/net/af_client", "r");
-    if (!fp){
+    if (!fp)
+    {
         printf("open dev file....failed\n");
         return;
     }
     fgets(line_buf, sizeof(line_buf), fp); // title
-    while(fgets(line_buf, sizeof(line_buf), fp)){
+    while (fgets(line_buf, sizeof(line_buf), fp))
+    {
         sscanf(line_buf, "%*s %s %s", mac_buf, ip_buf);
-        if (strlen(mac_buf) < 17){
+        if (strlen(mac_buf) < 17)
+        {
             printf("invalid mac:%s\n", mac_buf);
             continue;
         }
         dev_node_t *node = find_dev_node(mac_buf);
-        if (!node){
+        if (!node)
+        {
             node = add_dev_node(mac_buf);
             if (!node)
                 continue;
+            strncpy(node->ip, ip_buf, sizeof(node->ip));
         }
         node->online = 1;
     }
     fclose(fp);
 }
-void dump_dev_list(void){
+void dump_dev_list(void)
+{
     int i, j;
-	int count = 0;
-	clean_dev_online_status();
-	update_dev_hostname();
-	check_dev_expire();
-	FILE *fp = fopen(OAF_DEV_LIST_FILE, "w");
-	if (!fp){
-		return;
-	}
-		
-	fprintf(fp, "%-4s %-20s %-20s %-32s %-8s\n", "Id", "Mac Addr", "Ip Addr", "Hostname", "Online");
-    for (i = 0;i < MAX_DEV_NODE_HASH_SIZE; i++){
-        dev_node_t *node = dev_hash_table[i];
-		while(node){
-			if (node->online != 0){
-				if (strlen(node->hostname) == 0)
-					strcpy(node->hostname, "*");
-				fprintf(fp, "%-4d %-20s %-20s %-32s %-8d\n", 
-					i + 1, node->mac, node->ip, node->hostname, node->online);
-				count++;
-			}
-			if (count >= MAX_SUPPORT_DEV_NUM){
-				goto EXIT;
-			}
-			node = node->next;
-		}
+    int count = 0;
+    char hostname_buf[MAX_HOSTNAME_SIZE] = {0};
+    char ip_buf[MAX_IP_LEN] = {0};
+    clean_dev_online_status();
+    update_dev_hostname();
+    check_dev_expire();
+    FILE *fp = fopen(OAF_DEV_LIST_FILE, "w");
+    if (!fp)
+    {
+        return;
     }
 
-	for (i = 0;i < MAX_DEV_NODE_HASH_SIZE; i++){
+    fprintf(fp, "%-4s %-20s %-20s %-32s %-8s\n", "Id", "Mac Addr", "Ip Addr", "Hostname", "Online");
+    for (i = 0; i < MAX_DEV_NODE_HASH_SIZE; i++)
+    {
         dev_node_t *node = dev_hash_table[i];
-		while(node){
-
-			if (node->online == 0){
-				if (strlen(node->hostname) == 0)
-					strcpy(node->hostname, "*");
-				fprintf(fp, "%-4d %-20s %-20s %-32s %-8d\n", 
-					i + 1, node->mac, node->ip, node->hostname, node->online);
-			}
-			if (count >= MAX_SUPPORT_DEV_NUM)
-				goto EXIT;
-			node = node->next;
-		}
+        while (node)
+        {
+            if (node->online != 0)
+            {
+                if (strlen(node->hostname) == 0)
+                    strcpy(hostname_buf, "*");
+                else
+                    strcpy(hostname_buf, node->hostname);
+                if (strlen(node->ip) == 0)
+                    strcpy(ip_buf, "*");
+                else
+                    strcpy(ip_buf, node->ip);
+                fprintf(fp, "%-4d %-20s %-20s %-32s %-8d\n",
+                        i + 1, node->mac, ip_buf, hostname_buf, node->online);
+                count++;
+            }
+            if (count >= MAX_SUPPORT_DEV_NUM)
+            {
+                goto EXIT;
+            }
+            node = node->next;
+        }
     }
-EXIT:
-	fclose(fp);
-}
-
-void dump_dev_visit_list(void){
-    int i, j;
-	int count = 0;
-	FILE *fp = fopen(OAF_VISIT_LIST_FILE, "w");
-	if (!fp){
-		return;
-	}
-		
-	fprintf(fp, "%-4s %-20s %-20s %-8s %-32s %-32s %-32s\n", "Id", "Mac Addr", \
-		"Ip Addr", "Appid", "First Time", "Latest Time", "Total Time(s)");
-    for (i = 0;i < MAX_DEV_NODE_HASH_SIZE; i++){
+    for (i = 0; i < MAX_DEV_NODE_HASH_SIZE; i++)
+    {
         dev_node_t *node = dev_hash_table[i];
-        while(node){
-			for (j = 0; j < MAX_VISIT_HASH_SIZE; j++){
-				visit_info_t *p_info = node->visit_htable[j];
-				while(p_info){
-					char *first_time_str = format_time(p_info->first_time);
-					char *latest_time_str = format_time(p_info->latest_time);
-					int total_time = p_info->latest_time - p_info->first_time;
-					fprintf(fp, "%-4d %-20s %-20s %-8d %-32s %-32s %-32d\n", 
-						count, node->mac, node->ip, p_info->appid, first_time_str,
-						latest_time_str, total_time);
-					if (first_time_str)
-						free(first_time_str);
-					if (latest_time_str)
-						free(latest_time_str);
-					p_info = p_info->next;
-					count++;
-					if (count > 50)
-						goto EXIT;
-				}
-			}
-			node = node->next;
+        while (node)
+        {
+            if (node->online == 0)
+            {
+                if (strlen(node->hostname) == 0)
+                    strcpy(hostname_buf, "*");
+                else
+                    strcpy(hostname_buf, node->hostname);
+
+                if (strlen(node->ip) == 0)
+                    strcpy(ip_buf, "*");
+                else
+                    strcpy(ip_buf, node->ip);
+
+                fprintf(fp, "%-4d %-20s %-20s %-32s %-8d\n",
+                        i + 1, node->mac, ip_buf, hostname_buf, node->online);
+            }
+            if (count >= MAX_SUPPORT_DEV_NUM)
+                goto EXIT;
+            node = node->next;
         }
     }
 EXIT:
-	fclose(fp);
+    fclose(fp);
 }
 
+void dump_dev_visit_list(void)
+{
+    int i, j;
+    int count = 0;
+    FILE *fp = fopen(OAF_VISIT_LIST_FILE, "w");
+    if (!fp)
+    {
+        return;
+    }
+
+    fprintf(fp, "%-4s %-20s %-20s %-8s %-32s %-32s %-32s\n", "Id", "Mac Addr",
+            "Ip Addr", "Appid", "First Time", "Latest Time", "Total Time(s)");
+    for (i = 0; i < MAX_DEV_NODE_HASH_SIZE; i++)
+    {
+        dev_node_t *node = dev_hash_table[i];
+        while (node)
+        {
+            for (j = 0; j < MAX_VISIT_HASH_SIZE; j++)
+            {
+                visit_info_t *p_info = node->visit_htable[j];
+                while (p_info)
+                {
+                    char *first_time_str = format_time(p_info->first_time);
+                    char *latest_time_str = format_time(p_info->latest_time);
+                    int total_time = p_info->latest_time - p_info->first_time;
+                    fprintf(fp, "%-4d %-20s %-20s %-8d %-32s %-32s %-32d\n",
+                            count, node->mac, node->ip, p_info->appid, first_time_str,
+                            latest_time_str, total_time);
+                    if (first_time_str)
+                        free(first_time_str);
+                    if (latest_time_str)
+                        free(latest_time_str);
+                    p_info = p_info->next;
+                    count++;
+                    if (count > 50)
+                        goto EXIT;
+                }
+            }
+            node = node->next;
+        }
+    }
+EXIT:
+    fclose(fp);
+}
--- open-app-filter/src/main.c
+++ open-app-filter/src/main.c
@@ -1,23 +1,23 @@
 /*
-  Copyright (C) 2020 Derry <destan19@126.com>
+Copyright (C) 2020 Derry <destan19@126.com>
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
 #include <unistd.h>
 #include <stdlib.h>
@@ -30,82 +30,87 @@
 #include "appfilter_ubus.h"
 #include "appfilter_config.h"
 #include <time.h>
-void check_appfilter_enable(void){
-	int enable = 1;
-	struct tm *t;
-	time_t tt;
-	time(&tt);
+void check_appfilter_enable(void)
+{
+    int enable = 1;
+    struct tm *t;
+    time_t tt;
+    time(&tt);
     enable = config_get_appfilter_enable();
     if (0 == enable)
         goto EXIT;
-	af_ctl_time_t *af_t = load_appfilter_ctl_time_config();
-	if (!af_t){
-		enable = 0;
-		goto EXIT;
-	}
+    af_ctl_time_t *af_t = load_appfilter_ctl_time_config();
+    if (!af_t)
+    {
+        enable = 0;
+        goto EXIT;
+    }
 
-	t = localtime(&tt);
-	if (af_t->days[t->tm_wday] != 1){
-		enable = 0;
-		goto EXIT;
-	}
-	if (af_t->start.hour <= af_t->end.hour){
-		int cur_mins = t->tm_hour * 60 + t->tm_min;
-		if ((af_t->start.hour * 60 + af_t->start.min > cur_mins )
-			|| (cur_mins > af_t->end.hour * 60 + af_t->end.min)){
-			enable = 0;
-		}
-	}
-	else
-		enable = 0;
+    t = localtime(&tt);
+    if (af_t->days[t->tm_wday] != 1)
+    {
+        enable = 0;
+        goto EXIT;
+    }
+    if (af_t->start.hour <= af_t->end.hour)
+    {
+        int cur_mins = t->tm_hour * 60 + t->tm_min;
+        if ((af_t->start.hour * 60 + af_t->start.min > cur_mins) || (cur_mins > af_t->end.hour * 60 + af_t->end.min))
+        {
+            enable = 0;
+        }
+    }
+    else
+        enable = 0;
 EXIT:
-	if (enable){
-		system("echo 1 >/proc/sys/oaf/enable ");
-	}
-	else
-		system("echo 0 >/proc/sys/oaf/enable ");
-	free(af_t);
+    if (enable)
+    {
+        system("echo 1 >/proc/sys/oaf/enable ");
+    }
+    else
+        system("echo 0 >/proc/sys/oaf/enable ");
+    free(af_t);
 }
 
-void dev_list_timeout_handler(struct uloop_timeout *t){
+void dev_list_timeout_handler(struct uloop_timeout *t)
+{
     dump_dev_list();
-//	dump_dev_visit_list();
-	check_appfilter_enable();
-//todo: dev list expire
-	uloop_timeout_set(t, 10000);
+    //	dump_dev_visit_list();
+    check_appfilter_enable();
+    //todo: dev list expire
+    uloop_timeout_set(t, 10000);
 }
 
-struct uloop_timeout dev_tm={
-	.cb = dev_list_timeout_handler
-};
+struct uloop_timeout dev_tm = {
+    .cb = dev_list_timeout_handler};
 
 static struct uloop_fd appfilter_nl_fd = {
-	.cb = appfilter_nl_handler,
+    .cb = appfilter_nl_handler,
 };
 
 int main(int argc, char **argv)
 {
     int ret = 0;
-	uloop_init();
+    uloop_init();
     printf("init appfilter\n");
     init_dev_node_htable();
-	init_app_name_table();
-	init_app_class_name_table();
-	if (appfilter_ubus_init() < 0) {
-		fprintf(stderr, "Failed to connect to ubus\n");
-		return 1;
-	}
+    init_app_name_table();
+    init_app_class_name_table();
+    if (appfilter_ubus_init() < 0)
+    {
+        fprintf(stderr, "Failed to connect to ubus\n");
+        return 1;
+    }
 
     appfilter_nl_fd.fd = appfilter_nl_init();
     uloop_fd_add(&appfilter_nl_fd, ULOOP_READ);
-	af_msg_t msg;
-	msg.action = AF_MSG_INIT;
-	
-	send_msg_to_kernel(appfilter_nl_fd.fd, (void *)&msg, sizeof(msg));
-	uloop_timeout_set(&dev_tm, 5000);
-    uloop_timeout_add(&dev_tm);
-	uloop_run();
-	uloop_done();
-	return 0;
-}
+    af_msg_t msg;
+    msg.action = AF_MSG_INIT;
 
+    send_msg_to_kernel(appfilter_nl_fd.fd, (void *)&msg, sizeof(msg));
+    uloop_timeout_set(&dev_tm, 5000);
+    uloop_timeout_add(&dev_tm);
+    uloop_run();
+    uloop_done();
+    return 0;
+}
