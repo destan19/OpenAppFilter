87287dfb6fcd1bc782741b9b28343d81020bac8b
jjm2473 <jjm2473@gmail.com>
2025-05-29 13:05:02 +0800

    open-app-filter: move scripts that are not expected to be called by users to libexec/oaf

--- open-app-filter/src/main.c
+++ open-app-filter/src/main.c
@@ -376,7 +376,7 @@ void af_hnat_init(void){
     }
     if (g_hnat_init == 0){
         LOG_INFO("disable hnat...\n");
-        system("/usr/bin/hnat.sh");
+        system("/usr/libexec/oaf/hnat.sh");
         g_hnat_init = 1;
     }
 }
@@ -439,7 +439,7 @@ int af_load_feature_to_kernel(void){
 }
 
 int reload_feature(void){
-    system("gen_class.sh /tmp/feature.cfg");
+    system("/usr/libexec/oaf/gen_class.sh /tmp/feature.cfg");
     init_app_name_table();
     init_app_class_name_table();
     if (af_load_feature_to_kernel() < 0){
